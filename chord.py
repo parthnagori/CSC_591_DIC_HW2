7c0d972d1558f7491c68b2801e8015c267560117
Parth Nagori <nagori.parth@gmail.com>
2018-11-01 21:19:27 -0400

    initial functionality running properly

--- chord.py
+++ chord.py
@@ -3,75 +3,101 @@ import sys
 import argparse
 from itertools import cycle
 
-def is_id_in_range(start, end, val, left, right):
-  offset = (ring_length - start)%ring_length
-  start = (start+offset)%ring_length
-  end = (end+offset)%ring_length
-  val = (val+offset)%ring_length
-  if left and right:
-    return start <= val <= end
-  elif left:
-    return start <= val < end
-  elif right:
-    return start < val <= end
+# def is_id_in_range(start, end, val):
+#   print("is {} in [{},{}]".format(val,start,end))
+#   offset = (ring_length - start)%ring_length
+#   start = (start+offset)%ring_length
+#   end = (end+offset)%ring_length
+#   val = (val+offset)%ring_length
+#   print(start <= val <= end)
+#   return start <= val <= end
+
+def is_id_in_range(start,end,val):
+  start = start%ring_length
+  end = end%ring_length
+  if start <= end:
+    node_range = [i for i in range(start,end)]
+    return val in node_range
   else:
-    return start < val < end
+    node_range = set([i for i in range(0,end)] + [i for i in range(start,ring_length)])
+    return val in node_range
 
 def get_start(val, i):
-  return (val + (2**i))
+  return ( (val + (2**i)) % (2**m) )
 
 class Node:
   def __init__(self,val):
     self.id = val
-    self.predecessor = None
-    # self.finger[0] = self
+    self.predecessor = self
     self.finger = [self for i in range(m)]
 
   def join(self, n1):
     print("Inside join")
-    self.predecessor = None
-    self.finger[0] = n1.find_successor(self.id);
-    self.stabilize()
-    n1.stabilize()
-    # self.init_finger_table(n1)
-    # self.update_others()
+    self.init_finger_table(n1)
+    self.update_others()
     
   def init_finger_table(self, n1):
+    # print("init finger")
     finger_start = get_start(self.finger[0].id, 0)
     self.finger[0] = n1.find_successor(finger_start)
     self.predecessor = self.finger[0].predecessor
     self.finger[0].predecessor = self
     for i in range(0,m-1):
       curr_finger_start = get_start(self.finger[i+1].id, i+1)
-      if is_id_in_range(self.id, self.finger[i].id, curr_finger_start, True, False):
+      if is_id_in_range(self.id, self.finger[i].id, curr_finger_start):
         self.finger[i+1] = self.finger[i]
       else:
         self.finger[i+1] = n1.find_successor(curr_finger_start)
 
-  # def update_others(self):
-  #   for i in range(0,m):
-  #     p = self.find_predecessor()
+  def find_predecessor(self,val):
+    # print("find pre")
+    n1 = self
+    temp_val = n1.id
+    while not is_id_in_range(n1.id+1,n1.finger[0].id+1,val):
+      n1 = n1.closest_preceding_finger(val)
+      if n1.id == temp_val:
+        break
+      temp_val = n1.id
+    return n1
+
+  def update_finger_table(self, s, i):
+    # print("Updating finger table : {} --- {}".format(s,i))
+    if (is_id_in_range(self.id, self.finger[i].id, s.id)):
+      self.finger[i] = s
+      p = self.predecessor
+      # print("{} pred : {}".format(self.id, p))
+      p.update_finger_table(s,i)
+
+  def update_others(self):
+    # print("update others")
+    for i in range(0,m):
+      p = self.find_predecessor((self.id - (2**i))%(2**m))
+      # print("P : {}".format(p))
+      p.update_finger_table(self, i)
 
   def stabilize(self):
-    print("Inside stabilize")
+    # print("Inside stabilize")
+    if self.predecessor and self.predecessor.id not in ring:
+      self.predecessor = self
+    if self.finger[0] and self.finger[0].id not in ring:
+      self.finger[0] = self
+
     x = self.finger[0].predecessor
-    if x and is_id_in_range(self.id, self.finger[0].id, x.id, False, False):
+    if is_id_in_range(self.id+1, self.finger[0].id, x.id):
+      # print(x)
       self.finger[0] = x
     self.finger[0].notify(self)
-    # stabilize -- called periodically; node updates who it thinks its successor is.
 
   def notify(self, n1):
-    print("Inside notify")
-    if self.predecessor == None or (self.predecessor and is_id_in_range(self.predecessor.id, self.id, n1.id, False, False)):
+    # print("Inside notify for n - {} and n1 - {}".format(self.id, n1.id))
+    if not self.predecessor or (is_id_in_range(self.predecessor.id+1, self.id, n1.id)):
       self.predecessor = n1
-    # notify -- a node tells its successor that it exists so the successor can update it predecessor value.
 
   def fix_fingers(self):
-    print("Inside Fix Fingers")
+    # print("Inside Fix Fingers")
     for i in range(0,m):
-      self.finger[i] = self.find_successor(self.id + 2**i)
-      if i == 0:
-        self.finger[0] = self.finger[i]
+      curr_finger_start = get_start(self.id, i)
+      self.finger[i] = self.find_successor(curr_finger_start)
 
   def display(self):
     succ = self.finger[0].id if self.finger[0] else None
@@ -80,18 +106,19 @@ class Node:
     print("< Node {}: suc {}, pre {}: finger {}".format(self.id, succ, pred, finger_table))
 
   def find_successor(self, val):
-    if not (is_id_in_range(self.id, self.finger[0].id, val, False, True)):
-      return self.finger[0]
-    else:
-      n0 = self.finger[0].closest_preceding_finger(val)
-      if n0 == self:
-        return self
-      return n0.find_successor(val)
+    # print("Find successor {}".format(val))
+    n1 = self.find_predecessor(val)
+    return n1.finger[0]
+    # if (is_id_in_range(self.id+1, self.finger[0].id, val)):
+    #   return self.finger[0]
+    # else:
+    #   n0 = self.closest_preceding_finger(val)
+    #   return n0.find_successor(val)
 
   def closest_preceding_finger(self, val):
     for i in range(m-1,-1,-1):
       curr_finger_id = self.finger[i].id
-      if is_id_in_range(self.id, val, curr_finger_id, False, False):
+      if is_id_in_range(self.id+1, val, curr_finger_id):
         return self.finger[i]
     return self
 
@@ -103,58 +130,83 @@ def execute(method, value=None, value1=None):
     else:    
       node = Node(value)
       ring[value] = node
-      print("Added node {}".format(value))
+      # print("Added node {}".format(value))
+  
   elif method == "join" and value != None and value1 != None:
     n = ring[value]
     n1 = ring[value1]
     n.join(n1)
+  
   elif method == "drop":
-    print("Inside Drop")
-  elif method == "fix":
-    print("Inside Fix")
-    self.fix_fingers()
+    # print("Inside Drop")
+    for k,node in ring.items():
+      for i in range(m):
+        if node.finger[i].id == ring[value].id:
+          node.finger[i] = node
+    ring.pop(value)
+
+  elif method == "fix" and value != None:
+    # print("Inside Fix")
+    if value in ring:
+      node = ring[value]
+      node.fix_fingers()
+    else:
+      print("Node {} does not exist".format(value))
+  
   elif method == "stab" and value != None:
-    print("Inside Stab")
-    node = ring[value]
-    node.stabilize()
-  elif method == "show" and value:
-    index = (value%ring_length) - 1
-    node = ring[index]
-    if node:
+    # print("Inside Stab")
+    if value in ring:
+      node = ring[value]
+      node.stabilize()
+    else:
+      print("Node {} doesn't exist".format(value))
+  elif method == "show" and value != None:
+    if value in ring:
+      node = ring[value]
       node.display()
     else:
       print("Node {} does not exist".format(value))
+  
   elif method == "list":
     for key, node in ring.items():
       node.display()
 
   elif method == "end":
     return "end"
+  
   else:
     print("Invalid input")
 
 
-
-
 def parse_and_execute(instruction, m):
   operands = instruction.split(" ")
-  print(operands)
+  # print(operands)
+  
   if len(operands) == 3:
     method = operands[0].lower()
-    value1 = int(operands[1])
-    value2 = int(operands[2])
-    if value1 > (2**m - 1) or value2 > (2**m -1):
+    try:
+      value1 = int(operands[1])
+      value2 = int(operands[2])
+      if value1 > (2**m - 1) or value2 > (2**m -1):
+        return "invalid"
+    except ValueError:
       return "invalid"
-    execute(method, value1, value2)
+    return execute(method, value1, value2)
+  
   elif len(operands) == 2:
     method = operands[0].lower()
-    value = int(operands[1])
-    if value > (2**m -1):
+    try:
+      value = int(operands[1])
+      if value > (2**m -1):
+        return "invalid"
+    except ValueError:
       return "invalid"
-    execute(method, value)
+    return execute(method, value)
+  
   elif len(operands) == 1:
     method = operands[0].lower()
-    execute(method)
+    return execute(method)
+  
   else:
     return "invalid"
 
@@ -162,15 +214,15 @@ def parse_and_execute(instruction, m):
 def execute_file_instructions(filename, m):
   with open(filename) as f:
     for line in f:
-      print(line)
-      parse_and_execute(line,m)
+      print("> {}".format(line.strip()))
+      parse_and_execute(line.strip(),m)
 
 if __name__ == '__main__':
   m = 0
   ring_length = 0
   ring = {}
   args = sys.argv
-  print(args)
+  # print(args)
   if len(args) == 4:
     test_file = args[2]
     m = int(args[3])
@@ -180,7 +232,7 @@ if __name__ == '__main__':
     m = int(args[1])
     ring_length = int(2**m)
     while(True):
-      print("\n>")
+      print("\n>",end='')
       instruction = input()
       flag = parse_and_execute(instruction, m)
       if flag == "end":
